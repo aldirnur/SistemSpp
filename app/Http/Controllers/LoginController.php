96f49f630406ad85acc2ab9f1e52aac2d3f47786
Ristiandhi Arifin <Ristiandhiarifin@gmail.com>
2022-12-22 20:13:59 +0700

    First Commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+        $title = "login";
+        return view('auth.login',compact(
+            'title',
+        ));
+    }
+
+    public function login(Request $request){
+        $this->validate($request ,[
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+       $authenticate = auth()->attempt($request->only('email','password'));
+       if (!$authenticate){
+           return back()->with('login_error',"Username atau Password Salah!!");
+       }
+
+       return redirect()->route('dashboard');
+    }
+}
